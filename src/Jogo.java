18187e96a9345959911f26e381504af706ceec90
Adrie2016 <adriell.bezerra@ccc.ufcg.edu.br>
2016-08-11 19:10:59 -0300

    Adicionei o metodo registraJogada

--- src/Jogo.java
+++ src/Jogo.java
@@ -54,7 +54,7 @@ public class Jogo {
 		this.quantasVezesZerou = quantasVezesZerou;
 	}
 	
-	//Adicionei esse método
+	//Adicionei esse método.
 	public void registraJogada(int score, boolean zerou){
 		setQuantasVezesJogou(getQuantasVezesZerou()+1);
 		if(zerou == true){
