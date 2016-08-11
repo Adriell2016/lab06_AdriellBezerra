9143a0020894afcf2d583ad9eac5d67a8678bebf
Adriell2016 <adriell.bezerra@ccc.ufcg.edu.br>
2016-08-11 17:39:40 -0300

    Iniciei a criação da classe Jogo

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,4 +1,61 @@
 
 public class Jogo {
 
+	private String nome;
+	private double preco;
+	private int maiorScore;
+	private int quantasVezesJogou;
+	private int quantasVezesZerou;
+	
+	public Jogo(String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+		maiorScore = 0;
+		quantasVezesJogou = 0;
+		quantasVezesZerou = 0;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getMaiorScore() {
+		return maiorScore;
+	}
+
+	public void setMaiorScore(int maiorScore) {
+		this.maiorScore = maiorScore;
+	}
+
+	public int getQuantasVezesJogou() {
+		return quantasVezesJogou;
+	}
+
+	public void setQuantasVezesJogou(int quantasVezesJogou) {
+		this.quantasVezesJogou = quantasVezesJogou;
+	}
+
+	public int getQuantasVezesZerou() {
+		return quantasVezesZerou;
+	}
+
+	public void setQuantasVezesZerou(int quantasVezesZerou) {
+		this.quantasVezesZerou = quantasVezesZerou;
+	}
+	
+	public void registraJogada(int score, boolean zerou){
+		
+	}
 }
